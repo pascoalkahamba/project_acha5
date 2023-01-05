7c31ff2d34d3413eb171126a56b3f5f7763e3d3d
pascoalkahamba <pascoalkahamba25@gmail.com>
2023-01-05 14:18:59 +0100

    create the components Home and Footer and your contents

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import React from "react";
+import { Route, Routes } from "react-router";
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
+import Home from "./Components/Home";
 
 function App() {
-  return <div className="App">Pascoal Kahamba</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <p>Pascoal Kahamba</p>
+    </div>
+  );
+};
+
+export default Footer;
