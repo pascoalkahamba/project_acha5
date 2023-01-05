1e0d0df563ee1b23598c71e3a28ce6f494bc6c58
pascoalkahamba <pascoalkahamba25@gmail.com>
2023-01-05 14:24:38 +0100

    styling the component Home

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -3,23 +3,44 @@ import styles from "./Home.module.css";
 
 const Home = () => {
   return (
-    <section>
+    <section className={styles.container}>
       <div className={styles.content}>
-        <p>Exercicío Acha-5</p> <span>6.5.3 </span> Dois jogadores, cada um
-        pensa uma palavra de 5 letras diferentes, sem acentos. Um tenta
-        adivinhar primeiro a palavra que o outro escolheu. Alternadamente, cada
-        um submete ao outro uma palavra também de 5 letras, sem regras. O outro
-        vai informar quantas letras daquela palavra estão presentes na sua
-        palavra secreta. Com base nessa informação e, através de lógica, as
-        letras vão sendo eliminadas ou descobertas. Por exemplo, suponha que a
-        palavra secreta do jogador 1é"zinco". Se o jogador 2 disser "arara", o
-        jogador 1 informa "zero", jáque nem o "a" nem o "r" ocorrem em "zinco".
-        O jogador 2 então elimina as duas letras. Se a palavra dita for "ossos",
-        o jogador 1 informa "uma", que pode ser "o" ou "s". Neste ponto (e na
-        sua próxima jogada), o jogador 2 pode dizer "esses" e, ao ser informado
-        que házero ocorrências, elimina o "e" e o "s" e descobre em "ossos" que
-        a letra "o" pertenceàpalavra secreta do jogador 1. Implemente esse jogo,
-        com um banco de palavras de 5 letras para que o computador sorteie a sua
+        <h1>Exercicío Acha-5</h1>
+        <div className={styles.message}>
+          <span>6.5.3 </span> Dois jogadores, cada um pensa uma palavra de 5
+          letras diferentes, sem acentos. Um tenta adivinhar primeiro a palavra
+          que o outro escolheu. Alternadamente, cada um submete ao outro uma
+          palavra também de 5 letras, sem regras. O outro vai informar quantas
+          letras daquela palavra estão presentes na sua palavra secreta. Com
+          base nessa informação e, através de lógica, as letras vão sendo
+          eliminadas ou descobertas. Por exemplo, suponha que a palavra secreta
+          do jogador 1é"zinco". Se o jogador 2 disser "arara", o jogador 1
+          informa "zero", jáque nem o "a" nem o "r" ocorrem em "zinco". O
+          jogador 2 então elimina as duas letras. Se a palavra dita for "ossos",
+          o jogador 1 informa "uma", que pode ser "o" ou "s". Neste ponto (e na
+          sua próxima jogada), o jogador 2 pode dizer "esses" e, ao ser
+          informado que házero ocorrências, elimina o "e" e o "s" e descobre em
+          "ossos" que a letra "o" pertenceàpalavra secreta do jogador 1.
+          Implemente esse jogo, com um banco de palavras de 5 letras para que o
+          computador sorteie a sua palavra secreta
+        </div>
+      </div>
+      <div className={styles.form}>
+        <h1 className={styles.center}>Acha-5</h1>
+        letras diferentes, sem acentos. Um tenta adivinhar primeiro a palavra
+        que o outro escolheu. Alternadamente, cada um submete ao outro uma
+        palavra também de 5 letras, sem regras. O outro vai informar quantas
+        letras daquela palavra estão presentes na sua palavra secreta. Com base
+        nessa informação e, através de lógica, as letras vão sendo eliminadas ou
+        descobertas. Por exemplo, suponha que a palavra secreta do jogador
+        1é"zinco". Se o jogador 2 disser "arara", o jogador 1 informa "zero",
+        jáque nem o "a" nem o "r" ocorrem em "zinco". O jogador 2 então elimina
+        as duas letras. Se a palavra dita for "ossos", o jogador 1 informa
+        "uma", que pode ser "o" ou "s". Neste ponto (e na sua próxima jogada), o
+        jogador 2 pode dizer "esses" e, ao ser informado que házero ocorrências,
+        elimina o "e" e o "s" e descobre em "ossos" que a letra "o"
+        pertenceàpalavra secreta do jogador 1. Implemente esse jogo, com um
+        banco de palavras de 5 letras para que o computador sorteie a sua
         palavra secreta
       </div>
     </section>
